451c05ba79a75bf569ca0a6350b989db908648f9
SowmyaRavi29 <sowmyaravi0429@gmail.com>
2025-11-18 16:42:52 +0530

    moodAnalyzer

---
+++ MoodAnalysisException.java
@@ -0,0 +1,11 @@
+package moodAnalyzer;
+
+public class MoodAnalysisException extends Exception {
+    enum ExceptionType { NULL, EMPTY }
+    ExceptionType type;
+
+    public MoodAnalysisException(ExceptionType type, String message) {
+        super(message);
+        this.type = type;
+    }
+}
